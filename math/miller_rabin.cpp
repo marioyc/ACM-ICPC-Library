6f93763f02463e0a4c2f0682224747cc6d2cccea
marioyc <ycmario@gmail.com>
2013-05-04 09:47:07 -0500

    Quitando módulos innecesarios en miller_rabin

--- math/miller_rabin.cpp
+++ math/miller_rabin.cpp
@@ -1,11 +1,13 @@
 typedef unsigned long long ULL;
 
 ULL mulmod(ULL a, ULL b, ULL c){
-    ULL x = 0,y = a%c;
+    ULL x = 0,y = a % c;
 	
-    while(b>0){
-        if(b&1) x = (x+y)%c;
-        y = (y<<1)%c;
+    while(b > 0){
+        if(b & 1) x += y;
+        y <<= 1;
+        if(x >= c) x -= c;
+        if(y >= c) y -= c;
         b >>= 1;
     }
 	
@@ -13,10 +15,10 @@ ULL mulmod(ULL a, ULL b, ULL c){
 }
 
 ULL pow(ULL a, ULL b, ULL c){
-    ULL x = 1, y = a;
+    ULL x = 1,y = a;
 	
-    while(b>0){
-        if(b&1) x = mulmod(x,y,c);
+    while(b > 0){
+        if(b & 1) x = mulmod(x,y,c);
         y = mulmod(y,y,c);
         b >>= 1;
     }
@@ -25,27 +27,26 @@ ULL pow(ULL a, ULL b, ULL c){
 }
 
 bool miller_rabin(ULL p, int it){
-    if(p<2) return false;
-    if(p==2) return true;
-    if((p&1)==0) return false;
+    if(p < 2) return false;
+    if(p == 2) return true;
+    if((p & 1) == 0) return false;
 	
-    ULL s = p-1;
-    while(s%2==0) s >>= 1;
+    ULL s = p - 1;
+    while(s % 2 == 0) s >>= 1;
 	
     while(it--){
-        ULL a = rand()%(p-1)+1,temp = s;
+        ULL a = rand() % (p-1) + 1,temp = s;
         ULL mod = pow(a,temp,p);
 		
-        if(mod==-1 || mod==1) continue;
+        if(mod == -1 || mod == 1) continue;
 		
-        while(temp!=p-1 && mod!=p-1){
+        while(temp != p-1 && mod != p-1){
             mod = mulmod(mod,mod,p);
             temp <<= 1;
         }
 		
-        if(mod!=p-1) return false;
+        if(mod != p-1) return false;
     }
 	
     return true;
 }
-
