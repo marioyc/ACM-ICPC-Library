035bfd2693ea210421d119d9fc0cd51a5336bc96
Mario Ynocente Castro <ycmario@gmail.com>
2015-07-18 17:07:42 -0400

    Euler path/circuit for directed graphs

--- graphs/euler_directed.cpp
+++ graphs/euler_directed.cpp
@@ -1,37 +1,66 @@
-int V,E,to[32000],nxt[32000],last[1000],now[1000];
-int ans[32000];
+#define MAXV 5000
+#define MAXE 200000
 
-void init(){
-    memset(last,-1,sizeof(last));
-    E = 0;
-}
+struct DirectedEulerGraph {
+  int V,ne,last[MAXV],to[MAXE],next[MAXE],cur[MAXV];
+  int in[MAXV],out[MAXV];
+  int start,end;
+  vector<int> path;
 
-void make_edge(int u, int v){
-    to[E] = v; nxt[E] = last[u]; last[u] = E++;
-}
+  DirectedEulerGraph(){}
 
-// A : vertice inicial
-void euler(int A){
-    for(int i = 0;i < V;++i)
-        now[i] = last[i];
-    
-    stack<int> S;
-    S.push(A);
-    
-    int cur,sz = 0;
-    
-    while(!S.empty()){
-        cur = S.top();
-        
-        if(now[cur] != -1){
-            S.push(to[now[cur]]);
-            now[cur] = nxt[now[cur]];
-        }else{
-            ans[sz++] = cur;
-            S.pop();
-        }
+  void clear(int V_){
+    V = V_; ne = 0;
+    memset(last,-1,sizeof last);
+    memset(in,0,sizeof in);
+    memset(out,0,sizeof out);
+  }
+
+  void add_edge(int u, int v){
+    to[ne] = v; next[ne] = last[u]; last[u] = ne++;
+    ++out[u]; ++in[v];
+    start = u;
+  }
+
+  bool check(){
+    int cont = 0,aux = start;
+    start = end = -1;
+
+    for(int i = 0;i < V;++i){
+      if(in[i] == out[i]) ++cont;
+      else if(out[i] == in[i] + 1) start = i;
+      else if(in[i] == out[i] + 1) end = i;
+      else return false;
     }
-    
-    for(int i = sz - 1;i > 0;--i)
-        printf("%d %d\n",ans[i] + 1,ans[i - 1] + 1)
-}
+
+    if(cont == V){
+      start = end = aux;
+      return true;
+    }
+
+    return (cont == V - 2 && start != -1 && end != -1);
+  }
+
+  bool build(){
+    stack<int> S;
+    S.push(start);
+    memcpy(cur,last,sizeof last);
+    path.clear();
+
+    while(!S.empty()){
+      int u = S.top();
+
+      if(cur[u] == -1){
+        path.push_back(u);
+        S.pop();
+      }else{
+        int v = to[ cur[u] ];
+        cur[u] = next[ cur[u] ];
+        S.push(v);
+      }
+    }
+
+    reverse(path.begin(),path.end());
+    return path.size() == ne + 1;
+  }
+};
