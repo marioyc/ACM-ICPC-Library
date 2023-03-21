59e321e5a86dd86b60306b8ec6acae4471444467
MarioYc <marioyc>
2023-03-21 10:55:15 +0900

    move suffix array code to struct

--- strings/suffix_array/nlg^2n.cpp
+++ strings/suffix_array/nlg^2n.cpp
@@ -1,56 +1,56 @@
-#define MAXN 100000
-#define MAXLG 18
+#define SA_MAXN 1000005 // 100005
+#define SA_MAXLG 20 // 18
 
-char A[MAXN];
+struct sa_entry{
+    int nr[2],p;
 
-struct entry{
-	int nr[2],p;
-	
-	bool operator  < (entry X)const{
-		if(nr[0]==X.nr[0]) return nr[1]<X.nr[1];
-		return nr[0]<X.nr[0];
-	}
-}L[MAXN];
+    bool operator < (sa_entry &X)const{
+        if(nr[0] == X.nr[0]) return nr[1] < X.nr[1];
+        return nr[0] < X.nr[0];
+    }
+};
 
-int P[MAXLG][MAXN],N,step;
-int inv[MAXN];
+struct suffix_array{
+    char A[SA_MAXN];
+    sa_entry L[SA_MAXN];
+    int P[SA_MAXLG + 1][SA_MAXN],N,step;
+    // rank to position in A
+    int inv[SA_MAXN];
 
-void build_suffix_array(){
-    N = strlen(A);
-	for(int i=0;i<N;++i) P[0][i] = A[i];
-	
-	step = 1;
-	int cont = 1;
-	
-	while(cont<N){
-		for(int i=0;i<N;++i){
-			L[i].nr[0] = P[step-1][i];
-			L[i].nr[1] = (i+cont<N? P[step-1][i+cont]:-1);
-			L[i].p = i;
-		}
-		
-		sort(L,L+N);
-		
-		for(int i=0;i<N;++i){
-			if(i>0 && L[i].nr[0]==L[i-1].nr[0] && L[i].nr[1]==L[i-1].nr[1]) P[step][L[i].p] = P[step][L[i-1].p];
-			else P[step][L[i].p] = i;
-		}
-		
-		++step;
-		cont <<= 1;
-	}
-	
-	for(int i=0;i<N;++i) inv[P[step-1][i]] = i;
-}
+    void build(){
+        N = strlen(A);
+        step = 1;
+        int cursz = 1;
+        for(int i = 0;i < N;++i)
+            P[0][i] = A[i];
+        while(cursz < N){
+            for(int i = 0;i < N;++i){
+                L[i].nr[0] = P[step - 1][i];
+                L[i].nr[1] = (i + cursz < N? P[step - 1][i + cursz] : -1);
+                L[i].p = i;
+            }
+            sort(L, L + N);
+            for(int i = 0;i < N;++i){
+                if(i > 0 && L[i].nr[0] == L[i - 1].nr[0] && L[i].nr[1] == L[i - 1].nr[1]){
+                    P[step][L[i].p] = P[step][L[i - 1].p];
+                }else{
+                    P[step][L[i].p] = i;
+                }
+            }
+            ++step;
+            cursz <<= 1;
+        }
+        for(int i = 0;i < N;++i)
+            inv[P[step - 1][i]] = i;
+    }
 
-int lcp(int x, int y){
-	if(x==y) return N-x;
-	
-	int ret = 0;
-	
-	for(int k=step-1;k>=0 && x<N && y<N;--k)
-		if(P[k][x]==P[k][y])
-			x += (1<<k), y += (1<<k), ret |= (1<<k);
-	
-	return ret;
-}
+    // LCP for two suffix initial positions in string A
+    int lcp(int x, int y){
+        if(x == y) return N - x;
+        int ret = 0;
+        for(int k = step - 1;k >= 0 && x < N && y < N;--k)
+            if(P[k][x] == P[k][y])
+                x += (1<<k), y += (1<<k), ret |= (1<<k);
+        return ret;
+    }
+};
