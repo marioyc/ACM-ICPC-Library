360649f358745fd020d4a28b615cbe8ff1b8196c
Mario Ynocente Castro <ycmario@gmail.com>
2023-05-17 16:51:22 +0900

    update manacher

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -1,6 +1,9 @@
-void manacher(int n, const char s[], int p[]){
+// calculates radius from palindrome centers
+// even indices: odd length palindromes
+// odd indices: even length palindromes
+void manacher(int n, const char s[], int pal_r[]){
   for (int i = 0, j = 0, k = 0; i <= 2 * (n - 1); ++i){
-    int l = i < k ? min(p[j + j - i], (k - i) / 2) : 0;
+    int l = i < k ? min(pal_r[j + j - i], (k - i) / 2) : 0;
     int a = i / 2 - l, b = (i + 1) / 2 + l;
     
     while(0 <= a && b < n && s[a] == s[b]){
@@ -9,7 +12,7 @@ void manacher(int n, const char s[], int p[]){
       ++l;
     }
     
-    p[i] = l;
+    pal_r[i] = l;
     
     if(k < 2 * b){
       j = i;
