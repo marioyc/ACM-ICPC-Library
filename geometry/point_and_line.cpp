a3dfb8656664d3e80186343cd8bc2f4b2edd6419
Mario Ynocente Castro <ycmario@gmail.com>
2023-06-20 09:46:18 +0900

    update polar sort comparison

--- geometry/point_and_line.cpp
+++ geometry/point_and_line.cpp
@@ -11,8 +11,8 @@ struct point{
     }
 
     bool operator < (const point &p) const{
-        if(fabs(x-p.x)>eps) return x<p.x;
-        return y>p.y;
+        if(fabs(x - p.x) > eps) return x < p.x;
+        return y > p.y;
     }
 };
 
@@ -20,14 +20,14 @@ double cross(point a, point b){
     return a.x * b.y - a.y * b.x;
 }
 
+bool is_upper(point &a){
+    return a.y > 0 || (a.y == 0 && a.x >= 0);
+}
+
 bool polar_cmp(point a, point b){
-    if(a.x >= 0 && b.x < 0) return true;
-    if(a.x < 0 && b.x >= 0) return false;
-    if(a.x == 0 && b.x == 0){
-        if(a.y > 0 && b.y < 0) return false;
-        if(a.y < 0 && b.y > 0) return true;
-    }
-    return cross(a,b) > 0;
+    bool ua = is_upper(a),ub = is_upper(b);
+    if(ua != ub) return !ua;
+    return cross(a, b) > 0;
 }
 
 struct line{
@@ -37,6 +37,6 @@ struct line{
     
     line(point _p1, point _p2){
         p1 = _p1; p2 = _p2;
-        if(p1.x>p2.x) swap(p1,p2);
+        if(p1.x > p2.x) swap(p1, p2);
     }
 };
