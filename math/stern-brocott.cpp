395ced6c062f08a70a97752a6844393ccb810976
Mario Ynocente Castro <ycmario@gmail.com>
2023-12-20 17:14:53 +0900

    Update stern-brocott.cpp
    Add search in Stern-Brocot tree

--- math/stern-brocott.cpp
+++ math/stern-brocott.cpp
@@ -12,3 +12,40 @@ void build(int lnum = 0, int lden = 1, int rnum = 1, int rden = 1){
     
     build(a,b,rnum,rden);
 }
+
+template<typename T>
+struct SternBrocot{
+    T max_den;
+    
+    SternBrocot(T max_den) : max_den(max_den) {}
+
+    // search for closest approximation of p / q
+    // choose the smallest if two are equally close
+    pair<T, T> search(T p, T q, T lnum = 0, T lden = 1, T rnum = 1, T rden = 1){
+        while(true){
+            T mi_num = lnum + rnum;
+            T mi_den = lden + rden;
+            if(mi_den > max_den){
+                T d_l = p * lden * rden - lnum * q * rden;
+                T d_r = rnum * lden *  q - p * lden * rden;
+                if(d_l <= d_r) return {lnum, lden};
+                return {rnum, rden};
+            }
+            if(mi_num * q == mi_den * p) return {mi_num, mi_den};
+            else if(mi_num * q > mi_den * p){
+                if(p * lden == q * lnum) return {lnum, lden};
+                T k = (q * rnum - p * rden) / (p * lden - q * lnum);
+                k = min(k, (max_den - rden) / lden);
+                rnum = lnum * k + rnum;
+                rden = lden * k + rden;
+            }else{
+                if(rnum * q == p * rden) return {rnum, rden};
+                T k = (p * lden - q * lnum) / (rnum * q - p * rden);
+                k = min(k, (max_den - lden) / rden);
+                lnum = lnum + k * rnum;
+                lden = lden + k * rden;
+            }
+        }
+        return {-1, -1};
+    }
+};
